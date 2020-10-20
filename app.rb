92b8634f0d6ce2651b0e4762bb4d9819338ac719
elsstrange <ellenabstrange@gmail.com>
2020-10-20 21:55:50 +0100

    Passing feature spec for valid user sign up

--- app.rb
+++ app.rb
@@ -1,6 +1,27 @@
 require 'rack'
 require 'sinatra/base'
+require 'sinatra/activerecord'
+
+require_relative 'models/user'
 
 class MakersBnb < Sinatra::Base
+  # register Sinatra::ActiveRecordsExtension
   enable :sessions
+
+  get '/' do
+    @user = User.find_by(id: session[:user_id])
+    erb :index
+  end
+
+  get '/users/new' do
+    erb :users_new
+  end
+
+  post '/users' do
+    user = User.new(username: params[:username], email: params[:email])
+    user.password = params[:password]
+    user.save!
+    session[:user_id] = user.id
+    redirect '/'
+  end
 end
