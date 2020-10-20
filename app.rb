e46c415dbb60ae6ef21b5e5a7197998178f781ef
elsstrange <ellenabstrange@gmail.com>
2020-10-20 21:57:53 +0100

    Passing feature test for authenticating valid login

--- app.rb
+++ app.rb
@@ -24,4 +24,16 @@ class MakersBnb < Sinatra::Base
     session[:user_id] = user.id
     redirect '/'
   end
+
+  get '/sessions/new' do
+    erb :sessions_new
+  end
+
+  post '/sessions' do
+    user = User.find_by(username: params[:username])
+    if user.password == params[:password]
+      session[:user_id] = user.id
+    end
+    redirect '/'
+  end
 end
