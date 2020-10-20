a946520212d37e5be958f44154d25b3973480fe8
elsstrange <ellenabstrange@gmail.com>
2020-10-20 22:12:03 +0100

    Fixing incorrect expectation Should have expected to find a log in button, instead was expecting not to find one

--- spec/features/user_log_in_spec.rb
+++ spec/features/user_log_in_spec.rb
@@ -1,7 +1,9 @@
 feature 'existing user signing in' do
   let(:username) { 'Test' }
+  let(:wrong_username) { 'test' }
   let(:email) { 'makersbnb@example.com' }
   let(:password) { 'some_password' }
+  let(:wrong_password) { 'wrong password' }
   let!(:user) { User.new(username: username, email: email) }
   
   before do
@@ -20,5 +22,17 @@ feature 'existing user signing in' do
     expect(page).to have_content "Welcome, #{user.username}"
     expect(page).not_to have_link('Sign Up', href: '/users/new')
     expect(page).not_to have_link('Log In', href: '/sessions/new')
+  end
+
+  scenario 'user has invalid password credentials' do
+    visit '/'
+    click_on 'Log In'
+    expect(current_path).to eq '/sessions/new'
+    fill_in 'username', with: user.username
+    fill_in 'password', with: wrong_password
+    click_button 'Log In'
+    expect(current_path).to eq '/sessions/new'
+    expect(page).to have_content "Log in failed. Check your details and try again."
+    expect(page).to have_button('Log In')
   end
 end
