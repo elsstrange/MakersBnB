ff2381db4ff18499dfb734135a1b6b0e13aca39e
elsstrange <ellenabstrange@gmail.com>
2020-10-20 21:53:35 +0100

    Replace DB setup Using Rake with migration instructions

---
+++ db/migrate/20201020192134_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password_hash
+      t.string :phone
+    end
+  end
+end
