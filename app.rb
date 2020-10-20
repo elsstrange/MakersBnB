961baf6d49859ee775f4dc4287a3a6dcf2e39aed
elsstrange <ellenabstrange@gmail.com>
2020-10-20 22:15:19 +0100

    Passing feature test for authenitcating invalid username credentials

--- app.rb
+++ app.rb
@@ -37,13 +37,12 @@ class MakersBnb < Sinatra::Base
 
   post '/sessions' do
     user = User.find_by(username: params[:username])
-    if user.password == params[:password]
+    if user && user.password == params[:password]
       session[:user_id] = user.id
       redirect '/'
     else
       flash[:error] = ERRORS[:login]
       redirect '/sessions/new'
     end
-    
   end
 end
