918e52ffa4e92cde47cc55d8162d0ddb23eab4fa
elsstrange <ellenabstrange@gmail.com>
2020-10-21 18:56:34 +0100

    Added feature spec for logging out (not yet passing)

--- spec/features/user_logs_in_and out_spec.rb
+++ spec/features/user_logs_in_and out_spec.rb
@@ -22,6 +22,7 @@ feature 'existing user signing in' do
     expect(page).to have_content "Welcome, #{user.username}"
     expect(page).not_to have_link('Sign Up', href: '/users/new')
     expect(page).not_to have_link('Log In', href: '/sessions/new')
+    expect(page).to have_link('Log Out', href: '/sessions')
   end
 
   scenario 'user has invalid password credentials' do
@@ -46,5 +47,33 @@ feature 'existing user signing in' do
     expect(current_path).to eq '/sessions/new'
     expect(page).to have_content "Log in failed. Check your details and try again."
     expect(page).to have_button('Log In')
+  end
+end
+
+feature 'user logging out' do
+  let(:username) { 'Test' }
+  let(:wrong_username) { 'test' }
+  let(:email) { 'makersbnb@example.com' }
+  let(:password) { 'some_password' }
+  let(:wrong_password) { 'wrong password' }
+  let!(:user) { User.new(username: username, email: email) }
+  
+  before do
+    user.password = password
+    user.save!
+    visit '/'
+    click_on 'Log In'
+    expect(current_path).to eq '/sessions/new'
+    fill_in 'username', with: user.username
+    fill_in 'password', with: password
+    click_button 'Log In'
+  end
+
+  scenario 'logged in user logs out' do
+    click_on 'Log Out'
+    expect(current_path).to eq '/'
+    expect(page).not_to have_content "Welcome, #{user.username}"
+    expect(page).to have_link('Sign Up', href: '/users/new')
+    expect(page).to have_link('Log In', href: '/sessions/new')
   end
 end
