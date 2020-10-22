ab696d09b55f8689f4d3cb9a2ea0fa39c7eecbdf
elsstrange <ellenabstrange@gmail.com>
2020-10-22 16:22:34 +0100

    Small stylistic fixes From rubocop

--- spec/models/space_spec.rb
+++ spec/models/space_spec.rb
@@ -5,7 +5,7 @@ describe Space do
   let!(:user) { create(:user) }
   let!(:space) { described_class.new(name: 'A name', description: description, price: 50, host_id: user.id) }
   
-    describe '#cropped_description' do
+  describe '#short_description' do
     it 'returns the first n characters of the description, based on the number provided' do
       expect(space.short_description(50)).to eq description[0..49]
     end
