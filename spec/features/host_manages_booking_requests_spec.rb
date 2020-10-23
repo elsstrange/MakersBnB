57ed6d373a5f468dbedb8453969d85f80f4402cb
elsstrange <ellenabstrange@gmail.com>
2020-10-23 14:56:31 +0100

    Corrected errors in feature test for host viewing booking request Fixed remaining erroneous reference to cancelled booking. Fixed incorrect within matching term syntax.

--- spec/features/host_manages_booking_requests_spec.rb
+++ spec/features/host_manages_booking_requests_spec.rb
@@ -8,7 +8,7 @@ feature 'viewing booking requests for spaces' do
   let!(:guest) { create(:guest) }
   let!(:pending_booking) { create(:pending_booking, space_id: space1.id, guest_id: guest.id) }
   let!(:confirmed_booking) { create(:confirmed_booking, advance_date: 2, space_id: space2.id, guest_id: guest.id) }
-  let!(:cancelled_booking) { create(:rejected_booking, advance_date: 3, space_id: space2.id, guest_id: guest.id) }
+  let!(:rejected_booking) { create(:rejected_booking, advance_date: 3, space_id: space2.id, guest_id: guest.id) }
   
   scenario 'a host reviews the booking requests they have received for their spaces' do
     visit '/'
@@ -19,13 +19,14 @@ feature 'viewing booking requests for spaces' do
     click_on 'Bookings'
 
     expect(current_path).to eq '/bookings'
-    within("section.bookings-as-host#space#{space1.id}") do
+    # save_and_open_page
+    within("section.host-space#space#{space1.id}") do
       expect(page).to have_content space1.name
       expect(page).to have_content pending_booking.date.strftime('%d/%m/%Y')
       expect(page).to have_content "Request from #{guest.username}"
       expect(page).to have_content 'Pending'
     end
-    within("section.bookings-as-host#space#{space2.id}") do
+    within("section.host-space#space#{space2.id}") do
       expect(page).to have_content space2.name
       
       within("article#spacebooking#{confirmed_booking.id}") do
