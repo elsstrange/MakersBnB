fde14b8ff6a23228feea74fa83d8ab7d6e863fae
elsstrange <ellenabstrange@gmail.com>
2020-10-23 14:45:24 +0100

    Added and passed unit test for #guest method on Booking class Returns the booking guest as an instance of user

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,12 @@
 require_relative './space'
+require_relative './user'
 
 class Booking < ActiveRecord::Base
   def space(space_class = Space)
     space_class.find_by(id: space_id)
+  end
+  
+  def guest(user_class = User)
+    user_class.find_by(id: guest_id)
   end
 end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -1,14 +1,25 @@
 require './app/models/booking'
 
 describe Booking do
-  let(:space_class) { double("Space class") }
+  let!(:guest) { create(:user) }
   let!(:space) { create(:space) }
-  let(:booking) { create(:booking, space_id: space.id) }
+  let(:booking) { create(:booking, space_id: space.id, guest_id: guest.id) }
 
   describe '#space' do
+    let(:space_class) { double('Space class') }
+
     it 'calls .find_by on the Space class, with the booking space_id attribute' do
       expect(space_class).to receive(:find_by).with(id: space.id)
       booking.space(space_class)
+    end
+  end
+
+  describe '#guest' do
+    let(:user_class) { double('User class') }
+
+    it 'calls .find_by on the User class, with the booking guest_id attribute' do
+      expect(user_class).to receive(:find_by).with(id: guest.id)
+      booking.guest(user_class)
     end
   end
 end
