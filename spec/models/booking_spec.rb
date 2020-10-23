f1baca2b9c65df6c7a3ada6d897d8a685498b924
elsstrange <ellenabstrange@gmail.com>
2020-10-23 12:02:52 +0100

    Added unit test for looking up a space from a booking

---
+++ spec/models/booking_spec.rb
@@ -0,0 +1,14 @@
+require './app/models/booking'
+
+describe Booking do
+  let(:space_class) { double("Space class") }
+  let!(:space) { create(:space) }
+  let(:booking) { create(:booking, space_id: space.id) }
+
+  describe '#space' do
+    it 'calls .find_by on the Space class, with the booking space_id attribute' do
+      expect(space_class).to receive(:find_by).with(space.id)
+      booking.space(space_class)
+    end
+  end
+end
