18b2f042eab7a1a12137f605ea7f4fe44146cf4c
elsstrange <ellenabstrange@gmail.com>
2020-10-23 21:54:06 +0100

    Passing tests for confirming and rejecting booking requests

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,9 @@
 class MakersBnb < Sinatra::Base
+  STATUSES = {
+    :reject  => 'rejected',
+    :confirm => 'confirmed',
+  }
+  
   get '/bookings' do
     @user = User.find_by(id: session[:user_id])
     @guest_bookings = Booking.where(guest_id: session[:user_id])
@@ -10,5 +15,10 @@ class MakersBnb < Sinatra::Base
     space = Space.find_by(id: params[:space_id])
     flash.next[:acknowledgement] = "Your request to book #{space.name} on #{booking.date.strftime(DATE_FORMAT)} has been received."
     redirect '/'
+  end
+
+  patch '/bookings/:id' do
+    Booking.find_by(id: params[:id]).update(status: params[:status])
+    redirect '/bookings'
   end
 end
