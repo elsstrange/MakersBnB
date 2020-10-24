b05cb8e74ac689274854bac672c3d0eb18b2347e
elsstrange <ellenabstrange@gmail.com>
2020-10-24 13:47:41 +0100

    Passing feature tests for preventing bad sign-ups

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class MakersBnb < Sinatra::Base
-  post '/users' do
+  post '/users' do   
+    redirect '/users/new' if bad_sign_up?(params)
+    
     user = User.new(username: params[:username], email: params[:email])
     user.password = params[:password]
     user.save!
@@ -8,6 +10,20 @@ class MakersBnb < Sinatra::Base
   end
   
   get '/users/new' do
+    redirect '/' if session[:user_id]
     erb :users_new
-  end  
+  end
+  
+  def bad_sign_up?(params)
+    if params[:password] != params[:'password confirmation']
+      flash.next[:error] = ERRORS[:sign_up][:password_mismatch]
+      return true
+    elsif User.exists?(username: params[:username])
+      flash.next[:error] = ERRORS[:sign_up][:duplicate_username]
+      return true
+    elsif User.exists?(email: params[:email])
+      flash.next[:error] = ERRORS[:sign_up][:duplicate_email]
+      return true
+    end
+  end
 end
