eb037745de5ff929b0a7c55468db0671b61e5b2a
elsstrange <ellenabstrange@gmail.com>
2020-10-24 12:45:00 +0100

    Added feature tests for validating sign up details Not yet passing

--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -2,6 +2,7 @@ feature 'new user signing up' do
   let(:username) { 'Test' }
   let(:email) { 'makersbnb@example.com' }
   let(:password) { 'some_password' }
+  let(:other_password) { 'some_other_password' }
   
   scenario 'user has valid new sign up details' do
     visit '/'
@@ -17,4 +18,54 @@ feature 'new user signing up' do
     expect(page).not_to have_link('Sign Up', href: '/users/new')
     expect(page).not_to have_link('Log In', href: '/sessions/new')
   end
+
+  scenario 'user has mismatching passwords', :pending do
+    visit '/'
+    click_on 'Sign Up'
+    expect(current_path).to eq '/users/new'
+    fill_in 'username', with: username
+    fill_in 'email', with: new_email
+    fill_in 'password', with: password
+    fill_in 'password confirmation', with: other_password
+    click_button 'Sign Up'
+    expect(current_path).to eq '/users/new'
+    expect(page).to have_content 'Sign up failed: Passwords must match'
+    expect(page).not_to have_button('Log Out')
+  end
+
+
+  feature 'preventing duplicate sign-ups' do
+    let!(:persistent_user) { create(:user, username: username, email: email) }
+    let(:new_username) { 'NewTest' }
+    let(:new_email) { 'newmakersbnb@example.com' }
+  
+    scenario 'user tries username that already exists' do     
+      visit '/'
+      click_on 'Sign Up'
+      expect(current_path).to eq '/users/new'
+      fill_in 'username', with: username
+      fill_in 'email', with: new_email
+      fill_in 'password', with: password
+      fill_in 'password confirmation', with: password
+      click_button 'Sign Up'
+      expect(current_path).to eq '/users/new'
+      expect(page).to have_content 'Sign up failed: Username is already registered'
+      expect(page).not_to have_button('Log Out')
+    end
+
+    scenario 'user tries email that already exists', :pending do
+      visit '/'
+      click_on 'Sign Up'
+      expect(current_path).to eq '/users/new'
+      fill_in 'username', with: new_username
+      fill_in 'email', with: email
+      fill_in 'password', with: password
+      fill_in 'password confirmation', with: password
+      click_button 'Sign Up'
+      expect(current_path).to eq '/users/new'
+      expect(page).to have_content 'Sign up failed: Email is already registered'
+      expect(page).not_to have_button('Log Out')
+    end
+  end
 end
+
